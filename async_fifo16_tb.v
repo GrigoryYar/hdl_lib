f96923abed599935c21e1545f94db31c546732cd
GrigoryYar <gri.polushkin@gmail.com>
2022-07-13 16:24:56 +0200

    parameter in test added

--- async_fifo16_tb.v
+++ async_fifo16_tb.v
@@ -12,39 +12,48 @@
 
 module async_fifo16_tb();
 
-reg W_CLK = 0;
-reg R_CLK = 0;
-reg DIN = 0;
-reg DIN_DV = 0;
-wire DOUT;
+parameter FIFO_WIDTH = 10;
+
+reg  W_CLK = 0;
+reg  [FIFO_WIDTH-1:0] DIN = 0;
+reg  DIN_DV = 0;
+
+reg  R_CLK = 0;
+wire [FIFO_WIDTH-1:0] DOUT;
 wire DOUT_DV;
 
 integer i;
 
-parameter PERIOD_WR = 60;
-parameter PERIOD_RD = 17;
+parameter PERIOD_WR = 37;
+parameter PERIOD_RD = 36;
 
-always #(PERIOD_WR/2) W_CLK = ~W_CLK;
-always #(PERIOD_RD/2) R_CLK = ~R_CLK;
+always #(PERIOD_WR/2.0) W_CLK = ~W_CLK;
+always #(PERIOD_RD/2.0) R_CLK = ~R_CLK;
 
 initial begin
     #(10*PERIOD_WR);
-    DIN_DV = 1;
-    #(PERIOD_WR)
-    DIN_DV = 0;
+    @(posedge W_CLK)
+    #0.1 DIN_DV = 1;
+    @(posedge W_CLK)
+    #0.1 DIN_DV = 0;
 
-    #(100*PERIOD_WR);
-    for (i = 0; i < 15; i = i + 1 ) 
+    #(20*PERIOD_WR);
+    for (i = 1; i < 60; i = i + 1 ) 
     begin
-        DIN = !DIN;
+        @(posedge W_CLK) 
+        #0.1 DIN = i;
         DIN_DV = 1;
-        #(PERIOD_WR);
-        DIN_DV = 0;
-        #(PERIOD_WR);
+        //@(posedge W_CLK)
+        //#0.5 DIN_DV = 0;
     end
+    @(posedge W_CLK)
+    #0.1 DIN_DV = 0;
 end
 
 async_fifo16
+    #(
+        .WIDTH      (FIFO_WIDTH)
+    )
     async_fifo16_inst
     (
         .W_CLK      (W_CLK   ), // in,  u[1], write clock
@@ -56,19 +65,13 @@ async_fifo16
         .DOUT_DV    (DOUT_DV )  // out, u[1], output data valid
     );
 
-reg FIFO_OUT = 0;
 initial
 begin
     forever 
     begin
-        
         @(posedge R_CLK)
-    
         if(DOUT_DV)
-        begin
-            FIFO_OUT <= DOUT;
-            $display("FIFO_OUT",FIFO_OUT);
-        end
+            $display("FIFO_OUT: ", DOUT);
     end 
 end
 
